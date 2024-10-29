609cb0fe7fdee5a170b64b03124cb4da48728fc7
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-29 18:28:58 -0300

    basic styling & fake json data & card component

---
+++ src/components/NoteCard.jsx
@@ -0,0 +1,16 @@
+const NoteCard = ({ note }) => {
+    let position = JSON.parse(note.position);
+    const colors = JSON.parse(note.colors);
+    const body = JSON.parse(note.body);
+ 
+    return (
+        <div
+            className="card"
+            style={{
+                backgroundColor: colors.colorBody,
+            }}
+        >
+            {body}
+        </div>
+    );
+};
