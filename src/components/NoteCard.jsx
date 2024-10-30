a5c61b22bb5f0f4aaecb78ae1cd46e1d851df101
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-30 18:52:19 -0300

    cards grow and shrink according to the amount of text

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,9 +1,23 @@
+import { useRef, useEffect } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
     let position = JSON.parse(note.position);
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
+
+    const textAreaRef = useRef(null);
+
+    useEffect(() => {
+        autoGrow(textAreaRef.current);
+    }
+    , [textAreaRef.current]);
+
+    const autoGrow = (textarea) => {
+        const { current } = textAreaRef;
+        current.style.height = "auto";
+        current.style.height = current.scrollHeight + "px";
+    }
  
     return (
         <div className="card" 
@@ -21,8 +35,10 @@ const NoteCard = ({ note }) => {
             </div>
             <div className="card-body">
                 <textarea
+                ref={textAreaRef}
                     style={{color: colors.colorText}}
                     defaultValue={body}
+                    onInput={() => autoGrow(textAreaRef.current)}
                 >
 
                 </textarea>
