d53962e92cf55adc1a3bec56b0b66664442ca215
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-10-31 13:38:51 -0300

    useState for moving the cards

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,10 +1,14 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, useState } from "react";
 import Trash from "../icons/Trash";
 
 const NoteCard = ({ note }) => {
-    let position = JSON.parse(note.position);
+    const [position, setPositon] = useState(JSON.parse(note.position));
     const colors = JSON.parse(note.colors);
     const body = JSON.parse(note.body);
+
+    let mouseStartPos = { x: 0, y: 0 };
+ 
+    const cardRef = useRef(null);
 
     const textAreaRef = useRef(null);
 
@@ -13,14 +17,49 @@ const NoteCard = ({ note }) => {
     }
     , [textAreaRef.current]);
 
+
+
     const autoGrow = (textarea) => {
         const { current } = textAreaRef;
         current.style.height = "auto";
         current.style.height = current.scrollHeight + "px";
     }
+
+    const mouseDown = (e) => {
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+ 
+        document.addEventListener("mousemove", mouseMove);
+        document.addEventListener("mouseup", mouseUp);
+    }
+
+    const mouseMove = (e) => {
+        //1 - Calculate move direction
+        let mouseMoveDir = {
+            x: mouseStartPos.x - e.clientX,
+            y: mouseStartPos.y - e.clientY,
+        };
+     
+        //2 - Update start position for next move.
+        mouseStartPos.x = e.clientX;
+        mouseStartPos.y = e.clientY;
+     
+        //3 - Update card top and left position.
+        setPositon({
+            x: cardRef.current.offsetLeft - mouseMoveDir.x,
+            y: cardRef.current.offsetTop - mouseMoveDir.y,
+        });
+    };
+
+    const mouseUp = () => {
+        document.removeEventListener("mousemove", mouseMove);
+        document.removeEventListener("mouseup", mouseUp);
+    };
  
     return (
-        <div className="card" 
+        <div
+            ref={cardRef}
+            className="card" 
             style={{ 
                 backgroundColor: colors.colorBody,
                 left: `${position.x}px`,
@@ -30,8 +69,15 @@ const NoteCard = ({ note }) => {
 
         >
 
-            <div className="card-header" style={{backgroundColor: colors.colorHeder}}>
+            <div 
+                
+                className="card-header"
+                onMouseDown = { mouseDown }
+                style={{backgroundColor: colors.colorHeder}}
+            >
+
                 <Trash />
+            
             </div>
             <div className="card-body">
                 <textarea
